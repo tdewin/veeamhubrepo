37121fbfd9c0053aeb4c4fdf6a242fb67c475108
tdewin <tdewin@users.noreply.github.com>
2021-02-18 10:56:40 +0100

    getting the buildscript to work
    on a clean install

--- package/build.sh
+++ package/build.sh
@@ -1,6 +1,12 @@
-BUILDPATH=/home/veeamadmin/package
-rm $BUILDPATH/../dist/veeamhubrep*.deb
-cp $BUILDPATH/../src/veeamhubrepo $BUILDPATH/veeamhubrepo/usr/bin/
+BUILDPATH=$(pwd)
+DIST=$BUILDPATH/../dist
+SRC=$BUILDPATH/../src
+
+mkdir $DIST -p
+
+cp $SRC/veeamhubrepo $BUILDPATH/veeamhubrepo/usr/bin/
 dpkg-deb --build $BUILDPATH/veeamhubrepo
-mv $BUILDPATH/veeamhubrepo.deb $BUILDPATH/../dist/veeamhubrepo_noarch_latest.deb
+
+mv $BUILDPATH/veeamhubrepo.deb $DIST/veeamhubrepo_noarch_latest.deb
 cp $BUILDPATH/../dist/veeamhubrepo_noarch_latest.deb $BUILDPATH/../dist/veeamhubrepo_noarch_$(date +"Y_%m_%d").deb
+
