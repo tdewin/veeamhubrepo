bf9849900d33e9395e35e67c3f8eabd1fcabf64f
tdewin <tdewin@users.noreply.github.com>
2021-02-18 10:59:21 +0100

    alsmost
    almost

--- package/build.sh
+++ package/build.sh
@@ -8,5 +8,5 @@ cp $SRC/veeamhubrepo $BUILDPATH/veeamhubrepo/usr/bin/
 dpkg-deb --build $BUILDPATH/veeamhubrepo
 
 mv $BUILDPATH/veeamhubrepo.deb $DIST/veeamhubrepo_noarch_latest.deb
-cp $BUILDPATH/../dist/veeamhubrepo_noarch_latest.deb $BUILDPATH/../dist/veeamhubrepo_noarch_$(date +"Y_%m_%d").deb
+cp $BUILDPATH/../dist/veeamhubrepo_noarch_latest.deb $BUILDPATH/../dist/veeamhubrepo_noarch_$(date +"%Y_%m_%d").deb
 
